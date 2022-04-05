c8bea891c3458a4218944f0a239b44ab31a44b96
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-05 14:18:00 +0200

    playRound function

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ function computerPlay () {
     let comChoice = choices[Math.floor(Math.random()*3)];
     return comChoice;
 }
-console.log(computerPlay())
+//console.log(computerPlay())
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {return 'Draw'}
+    switch (playerSelection) {
+        case 'rock':
+              if(computerSelection === 'paper') return 'You Lose! Paper beats Rock';
+              if(computerSelection === 'scissors') return 'You Win! Rock beats Scissors';
+              break;
+        case 'paper':
+            if(computerSelection === 'rock') return 'You Win! Paper beats Rock';
+            if(computerSelection === 'scissors') return 'You Lose! Scissors beats Paper';
+            break;
+        case 'scissors':
+            if(computerSelection === 'paper') return 'You Win! Scissors beats Paper';
+            if(computerSelection === 'rock') return 'You Lose! Rock beats Scissors';
+            break;
+        default: 
+            return 'You need to choose a valid selection';
+    }
+}
