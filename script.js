35431aa53afa2bd18167666c0b3c80af1b702516
medblutex <47444914+medblutex@users.noreply.github.com>
2022-04-06 01:54:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,13 +89,7 @@ function choose (e) {
         endGame.appendChild(winner())
         
     }
-    lastComScore = com;
-    lastPlayerScore = player;
 }
-let lastPlayerScore;
-let lastComScore;
-console.log(com);
-console.log(lastPlayerScore)
 function winner () {
     let winner = document.createElement('div');
     winner.className = 'winner';
@@ -111,36 +105,3 @@ function winner () {
     return winner;
 }
 
-// function game () {
-//     let round = 1
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         round++
-//         let playerSelection = (prompt('Rock, Paper, Scissors?', '')).trim().toLowerCase();
-//         let computerSelection = computerPlay();
-//         if (playRound(playerSelection, computerSelection).includes('Win!')) {
-//             playerScore++;
-//         } else if (playRound(playerSelection, computerSelection).includes('Lose!')) {
-//             computerScore++
-//         } else if (playRound(playerSelection, computerSelection).includes('Draw')) {
-//             playerScore++;
-//             computerScore++;
-//         } else if (playRound(playerSelection, computerSelection).includes('valid')) {
-//             return 'You need to choose a valid selection';
-//         }
-//         console.log(`Player : ${playerScore}`)
-//         console.log(`Computer : ${computerScore}`)
-
-//     }
-//     let winner;
-//     if (playerScore > computerScore) {
-//         winner = 'You Win The Game';
-//     } else if (playerScore < computerScore) {
-//         winner = 'You Lose The Game';
-//     } else if (playerScore === computerScore) {
-//         winner = 'Draw';
-//     }
-//     return winner;
-// }
-// console.log(game())
