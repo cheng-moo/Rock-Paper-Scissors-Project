5f6a2c387b4ff8a42f19ede55768a434756e97c0
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-05 14:57:52 +0200

    Game of 5 Rounds function

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ function playRound (playerSelection, computerSelection) {
             return 'You need to choose a valid selection';
     }
 }
+// const playerSelection = 'rock';
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
+function game () {
+    let round = 1
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        round++
+        let playerSelection = (prompt('Rock, Paper, Scissors?', '')).trim().toLowerCase();
+        let computerSelection = computerPlay();
+        if (playRound(playerSelection, computerSelection).includes('Win!')) {
+            playerScore++;
+        } else if (playRound(playerSelection, computerSelection).includes('Lose!')) {
+            computerScore++
+        } else if (playRound(playerSelection, computerSelection).includes('Draw')) {
+            playerScore++;
+            computerScore++;
+        } else if (playRound(playerSelection, computerSelection).includes('valid')) {
+            return 'You need to choose a valid selection';
+        }
+        console.log(`Player : ${playerScore}`)
+        console.log(`Computer : ${computerScore}`)
+
+    }
+    let winner;
+    if (playerScore > computerScore) {
+        winner = 'You Win The Game';
+    } else if (playerScore < computerScore) {
+        winner = 'You Lose The Game';
+    } else if (playerScore === computerScore) {
+        winner = 'Draw';
+    }
+    return winner;
+}
+console.log(game())
